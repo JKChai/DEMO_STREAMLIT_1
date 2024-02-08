fd5b45d22e35a6c1511152862a253043b3f72c0b
TTI\JChai <chaijinkun@gmail.com>
2024-02-08 08:32:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from pkgs.config import SetupFactory
 
 @st.cache_data
 def get_data():
-    df = pd.read_csv(".\\assets\\ContosoRetailDW_FactSales.csv")
+    rootpath = SetupFactory()
+    df = pd.read_csv(f"{rootpath}\\..\\assets\\ContosoRetailDW_FactSales.csv")
     return df
 
 def load_streamlit(obj):
